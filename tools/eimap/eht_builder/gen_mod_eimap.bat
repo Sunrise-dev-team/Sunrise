f7a1ef003cdce3b88ce3d60ed010fd70f116e86f
Atom-FG <Atom-FG@users.noreply.github.com>
2026-04-29 05:56:56 +0300

    Default tiles for eimp

--- tools/eimap/eht_builder/gen_mod_eimap.bat
+++ tools/eimap/eht_builder/gen_mod_eimap.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /D %~dp0
 set DONT_CHANGE_CWD=1
-python\python.exe -B src\gen_mod_eimap.py %*
+python.exe -B src\gen_mod_eimap.py %*
--- tools/eimap/eht_builder/gen_tileset_eimap.bat
+++ tools/eimap/eht_builder/gen_tileset_eimap.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /D %~dp0
 set DONT_CHANGE_CWD=1
-python\python.exe -B src\gen_tileset_eimap.py %*
+python.exe -B src\gen_tileset_eimap.py %*
 
